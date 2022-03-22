6f4080c501984d2a5272dfde5c11aa14869e08cf
Agustin Mansilla <agustinmansilla240@gmail.com>
2022-03-22 12:17:40 -0300

    add environment var

--- users/api.ts
+++ users/api.ts
@@ -4,8 +4,7 @@ import Papa from "papaparse";
 import {IUser, IUserRaw} from "./types";
 import {normalizeData} from "./utils";
 
-const BaseURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWYhC8EIHLS0IbB-86cHxVEEx_86P7kp9DJj5mwEYcL8z3jcWUOYh3JYy62v587AhHvUVg7WUNlUmp/pub?gid=1141353787&single=true&output=csv";
+const BaseURL = process.env.NEXT_PUBLIC_GOOGLE_SHEET as string;
 
 const api = {
   list: async (): Promise<IUser[]> => {
